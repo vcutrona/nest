1a975589b7a0572cf61a5cfbed5da7a48537c704
GPuleri <g.puleri@campus.unimib.it>
2020-11-09 16:58:14 +0100

    hybrid updated

--- hybrid/embeddings.py
+++ hybrid/embeddings.py
@@ -11,96 +11,125 @@ from collections import defaultdict
 nltk.download('stopwords')
 
 
-def create_surfaceFormIndex():
-    return
+def sfi_to_ste(surface_form_index):
+    """
+    Convert a surface_form_index in a more readable and quickly accessible surface_to_entities.
+    High speeding of candidates generation.
 
+    e.g.:
+        surfaceFormIndex = <http://dbpedia.org/resource/Italy> ['italy', 'italia'] 554
+                            <http://dbpedia.org/resource/Roma> ['italy', 'italia', 'roma'] 266
+        surfaceToEntities = {'italy': {'<http://dbpedia.org/resource/Italy>': 554,
+                                        '<http://dbpedia.org/resource/Roma>': 266}
+                            'italia': {'<http://dbpedia.org/resource/Italy>': 554,
+                                        '<http://dbpedia.org/resource/Roma>': 266}
+                            'roma':   {'<http://dbpedia.org/resource/Roma>': 266}}
 
-def surfaceFormIndex_to_surfaceToEntities(surface_form):
-    # converti surfaceFormIndex (csv) in surfaceToEntities (dict)
-    tempDictCandidates = defaultdict(lambda: defaultdict(int))
-    with open(surface_form, 'r', newline='', encoding='utf-8') as surface_form_index:
-        reader = csv.reader(surface_form_index, delimiter=' ')
+    :param surface_form_index: a csv surface_form_index
+    :return: a dict surface_to_entities
+    """
+    dict_candidates = defaultdict(lambda: defaultdict(int))
+    with open(surface_form_index, 'r', newline='', encoding='utf-8') as surface_form_index_file:
+        reader = csv.reader(surface_form_index_file, delimiter=' ')
         next(reader)
 
-        tempCandidates = [[sf_form.lower(), entity, count] for entity, sf_forms, count in reader
-                          for sf_form in ast.literal_eval(sf_forms)]
+        candidates = [[sf_form.lower(), entity, count] for entity, sf_forms, count in reader
+                      for sf_form in ast.literal_eval(sf_forms)]
 
-        surface_form_index.close()
+        surface_form_index_file.close()
 
-    for sf_form, entity, count in tempCandidates:
-        tempDictCandidates[sf_form][entity] = count
+    for sf_form, entity, count in candidates:
+        dict_candidates[sf_form][entity] = count
 
     return {sf_form: {entity: count[entity] for entity in count}
-            for sf_form, count in tempDictCandidates.items()}
+            for sf_form, count in dict_candidates.items()}
 
 
-def save(obj, save_to):
+def save(instance, save_to):
+    """
+    Save an instance of an object on disk as save_to
+
+    :param instance: the instance to save
+    :param save_to: the path where save the instance
+    :return:
+    """
     with open(save_to, 'wb') as file:
-        pickle.dump(obj, file, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(instance, file, protocol=pickle.HIGHEST_PROTOCOL)
         file.close()
 
 
 def load(load_from):
+    """
+    Load an instance of an object from load_from
+
+    :param load_from: the path from where to load
+    :return: the instance
+    """
     with open(load_from, 'rb') as file:
-        obj = pickle.load(file)
+        instance = pickle.load(file)
         file.close()
-    return obj
+    return instance
 
 
 def generate_candidates(labels, surface_to_entities, ng):
+    """
+    Generate possible candidates for all the labels.
+    It compares the labels to those stored in the surface_to_entities dict. All entities in the surface_to_entities
+    that provide an exact matching with the label serve as entity candidate. If no candidates are retrieved it
+    computes stemming and stopwords removal on the label and it compares the new label to those stored in the
+    surface_to_entities dict by applying trigram similarity; all entities that overcome the threshold serve as entity
+    candidates.
+    entity_candidates is a dict where keys k are tuple (index, sf_form) and values are graph which nodes are candidates.
+    Maintaining k different graph is very useful to easily create a disambiguation graph in the disambiguation step.
+
+    :param labels: a list of labels
+    :param surface_to_entities: the surface_to_entities dict
+    :param ng: the ngram model
+    :return: an entity_candidates dict
+    """
     entity_candidates = {}
     porter = PorterStemmer()
     tokenizer = nltk.RegexpTokenizer(r"\w+")
     stop_words = set(stopwords.words('english'))
-    # ng = NGram([surfaceForm for surfaceForm in surface_to_entities.keys()])
 
+    index = 0  # index needed since label might be repeated
     for label in labels:
         graph = nx.DiGraph()
-        candidates = []
         if label.lower() in surface_to_entities:
-            candidates = [(entity, {'weight': surface_to_entities[label.lower()].get(entity)})
+            candidates = [(entity, {'weight': int(surface_to_entities[label.lower()].get(entity))})
                           for entity in surface_to_entities[label.lower()]]
-            # candidates = [entity for entity in surfaceToEntities[label] if entity in w2v]
-        # else:  # se non c'e exact matching con label
-        #     word_tokens = [porter.stem(word) for word in tokenizer.tokenize(label.lower())]  # stemming
-        #     labelMod = [word for word in word_tokens if word not in stop_words]  # stopwords
-        #     labelMod = " ".join(labelMod)
-        #     listSF = [word for (word, sim) in ng.search(labelMod, 0.82)]  # trigram sim.
-        #     candidates = [(entity, {'weight': surface_to_entities[sf_form].get(entity)})
-        #                   for sf_form in listSF for entity in surface_to_entities[sf_form]]
-        #     # candidates = [entity for sf_form in listSF for entity in surfaceToEntities[sf_form] if entity in w2v]
+        else:  # se non c'e exact matching con label
+            word_tokens = [porter.stem(word) for word in tokenizer.tokenize(label.lower())]  # stemming
+            label_mod = [word for word in word_tokens if word not in stop_words]  # stopwords
+            label_mod = " ".join(label_mod)
+            listSF = [word for (word, sim) in ng.search(label_mod, 0.82)]  # trigram sim.
+            candidates = [(entity, {'weight': surface_to_entities[sf_form].get(entity)})
+                          for sf_form in listSF for entity in surface_to_entities[sf_form]]
+
         graph.add_nodes_from(candidates)
-        entity_candidates[label] = graph
+        entity_candidates[(index, label)] = graph  # index needed since label might be repeated
+        index += 1
 
     return entity_candidates
 
 
-def disambiguate_entities(entity_candidates, w2v):
-    disambiguation_graph = nx.DiGraph()
-
-    for surface_form in entity_candidates:
-        graph = nx.DiGraph()
-        # remove node if it is not in load_from(w2v)
-        nodes = filterNodes(surface_form, entity_candidates, w2v)
-        # take the best 8 candidates for each surface form
-        nodes = topCandidates(nodes)
-
-        graph.add_nodes_from(nodes)
-        entity_candidates[surface_form] = graph
-        disambiguation_graph = nx.compose(disambiguation_graph, entity_candidates.get(surface_form))
-
-    return disambiguation_graph, entity_candidates
-
-
 def create_subset(entity_candidates, load_from, save_to):
+    """
+    TO DELETE
+
+    :param entity_candidates:
+    :param load_from:
+    :param save_to:
+    :return:
+    """
     list_ = []
     with open(load_from, 'r', newline='', encoding='utf-8') as file:
         with open(save_to, 'w', newline='', encoding='utf-8') as filew:
             writer = []
             for line in file:
                 words = line.split()
-                for sf in entity_candidates:
-                    if words[0] in entity_candidates.get(sf) not in list_:
+                for (index, sf) in entity_candidates:
+                    if words[0] in entity_candidates.get((index, sf)) not in list_:
                         list_.append(words[0])
                         writer.append(line)
             filew.write(str(len(writer)) + " 300\n")
@@ -111,59 +140,121 @@ def create_subset(entity_candidates, load_from, save_to):
     return save_to
 
 
-def filterNodes(surface_form, entity_candidates, w2v):
-    nodes = []
-    for (entity, weight) in entity_candidates.get(surface_form).nodes(data=True):
-        if entity in w2v:
-            nodes.append((entity, {'weight': int(weight['weight'])}))
-    return nodes
+def disambiguate_entities(entity_candidates, w2v):
+    """
+    Create a complete directed k-partite disambiguation graph where k is the number of surface forms.
+    Since the number of entity candidates could be extremely high, a filtering is applied.
+
+    :param entity_candidates: the entity_candidates dict
+    :param w2v: an embeddings model
+    :return: the disambiguation graph, the entity_candidates updated after the filtering
+    """
+    disambiguation_graph = nx.DiGraph()
+
+    for (index, surface_form) in entity_candidates:
+        graph = nx.DiGraph()
+        nodes = filter_candidates((index, surface_form), entity_candidates, w2v)
+        graph.add_nodes_from(nodes)
+        entity_candidates[(index, surface_form)] = graph
+        disambiguation_graph = nx.compose(disambiguation_graph, entity_candidates.get((index, surface_form)))
+
+    return disambiguation_graph, entity_candidates
 
 
-def topCandidates(nodes, n=8):
-    return [(entity, {'weight': int(weight['weight'])})
-            for entity, weight in sorted(nodes, key=lambda item: item[1]['weight'], reverse=True)[:n]]
+def filter_candidates(index_surface_form, entity_candidates, w2v, n=8):
+    """
+    Filter candidates by taking only entities contained in the embeddings model w2v and reducing the number
+    of candidates to the n most relevant (highest priors probability) candidates.
+
+    :param index_surface_form: a tuple (index, surface_form)
+    :param entity_candidates: the entity_candidates dict
+    :param w2v: the embeddings model
+    :param n: the number of candidates to return
+    :return: a list of weighted entity candidates
+    """
+    return [(entity, {'weight': weight['weight']})
+            for entity, weight in
+            sorted(entity_candidates.get(index_surface_form).nodes(data=True),
+                   key=lambda item: item[1]['weight'], reverse=True)
+            if entity in w2v][:n]
 
 
-def addEdges(entity_candidates, disambiguation_graph, w2v):
-    for surface_form in entity_candidates:
-        for v1 in entity_candidates.get(surface_form):
-            for v2 in (set(disambiguation_graph.nodes()) - set(entity_candidates.get(surface_form))):
-                disambiguation_graph.add_weighted_edges_from([(v1, v2, abs(w2v.similarity(v1, v2)))])
+def add_edges(entity_candidates, disambiguation_graph, w2v):
+    """
+    Add weighted edges among the nodes in the disambiguation graph. Edges' weights are the cosine similarity between
+    the nodes which it is connected. Only positive weights are considered.
+
+    :param entity_candidates: the entity_candidates dict
+    :param disambiguation_graph: the disambiguation graph
+    :param w2v: the embeddings model
+    :return: the updated disambiguation graph
+    """
+    for (index, surface_form) in entity_candidates:
+        for v1 in entity_candidates.get((index, surface_form)):
+            for v2 in (set(disambiguation_graph.nodes()) - set(entity_candidates.get((index, surface_form)))):
+                if w2v.similarity(v1, v2) > 0:
+                    disambiguation_graph.add_weighted_edges_from([(v1, v2, w2v.similarity(v1, v2))])
     return disambiguation_graph
 
 
-def normalizePriors(entity_candidates):
+def normalize_priors(entity_candidates):
+    """
+    Normalize priors probability of the graph for each surface_form.
+
+    :param entity_candidates: the entity_candidates dict
+    :return: a dict {node: weight} where the sum of the nodes' weights belonging to the same surface form is 1.
+    """
     denominator = {}
-    for surface_form in entity_candidates:
-        denominator[surface_form] = 0
-        for (node, prop) in entity_candidates.get(surface_form).nodes(data=True):
-            denominator[surface_form] += prop['weight']
-    return denominator
+    for (index, surface_form) in entity_candidates:
+        denominator[(index, surface_form)] = 0
+        for (entity, prop) in entity_candidates.get((index, surface_form)).nodes(data=True):
+            denominator[(index, surface_form)] += prop['weight']
+
+    return {entity: prop['weight'] / denominator[(index, surface_form)]
+            for (index, surface_form) in entity_candidates
+            for (entity, prop) in entity_candidates.get((index, surface_form)).nodes(data=True)
+            if denominator[(index, surface_form)] != 0}
 
 
-def etp(graph, v1, v2, denominator):
-    return graph.get_edge_data(v1, v2)['weight'] / denominator
+def thin_out(disambiguation_graph):
+    """
+    Thin out 25% of those edges which weights are the lowest
+
+    :param disambiguation_graph: the disambiguation graph
+    :return: a list of the lowest weights edges to remove
+    """
+    n = int(0.75 * len(disambiguation_graph.edges.data("weight")))
+    return sorted(disambiguation_graph.edges.data("weight"), key=lambda item: item[2])[:n]
 
 
-def thinOut(graph):
-    count = int(0.75 * len(graph.edges.data("weight")))
-    return sorted(graph.edges.data("weight"), key=lambda tup: tup[2])[:count]
+def best_annotation(entity_candidates, page_rank):
+    """
+    Take the best candidate for each surface form provided by the page_rank execution
 
-
-def bestAnnotation(entity_candidates, page_rank):
+    :param entity_candidates: the entity_candidates dict
+    :param page_rank: the scored provided after the page rank execution
+    :return: a list of annotations
+    """
     annotations = {}
-    for surface_form in entity_candidates:
+    for (index, surface_form) in entity_candidates:
         best = -1
-        annotations[surface_form] = "not annotated"
-        for entity in entity_candidates.get(surface_form):
+        annotations[(index, surface_form)] = "not annotated"
+        for entity in entity_candidates.get((index, surface_form)):
             if page_rank[entity] > best:
                 best = page_rank[entity]
-                annotations[surface_form] = entity
+                annotations[(index, surface_form)] = entity
 
     return annotations.values()
 
 
 def annotate(table, save_to):
+    """
+    Save the table in save_to
+
+    :param table: the annotated table
+    :param save_to: the path where save the table
+    :return:
+    """
     with open(save_to, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=' ', quoting=csv.QUOTE_ALL)
         for tab, col, row, annotation in table.itertuples(index=False):
--- hybrid/run_embeddings.py
+++ hybrid/run_embeddings.py
@@ -5,83 +5,49 @@ from gensim.models import KeyedVectors
 from ngram import NGram
 
 from factbase_lookup import sortTable, getLabelColumn
-# from hybrid.factbase_lookup import sortTable, getLabelColumn
 
-from embeddings import etp, thinOut, surfaceFormIndex_to_surfaceToEntities, save, load, \
-    generate_candidates, create_subset, disambiguate_entities, addEdges, normalizePriors, bestAnnotation, annotate
-"""from hybrid.embeddings import etp, thinOut, surfaceFormIndex_to_surfaceToEntities, save, load, \
-    generate_candidates, create_subset, disambiguate_entities, addEdges, normalizePriors, bestAnnotation, annotate"""
+from embeddings import sfi_to_ste, save, load, generate_candidates, create_subset, \
+    disambiguate_entities, add_edges, normalize_priors, thin_out, best_annotation, annotate
 
-# base_dir = '../hybrid/'
 base_dir = '/datahdd/gpuleri/'
-# datasets = '../datasets/T2D/targets/'
 datasets = '/datahdd/gpuleri/T2D/targets/'
-target = datasets + 'T2D_embedding.csv'
+target = datasets + 'T2D_embedding.csv'  # CEA_T2D_Targets.csv
 
 start = time.time()
-table = pd.read_csv(sortTable(target, 'table'))  # get targets
-labelColumn = getLabelColumn(table)  # get labels
+table = pd.read_csv(sortTable(target, 'table'))
+label_column = getLabelColumn(table)
 annotated_table = table.copy()
 annotations = []
-#surfaceToEntities = surfaceFormIndex_to_surfaceToEntities(base_dir + 'surfaceFormIndex.csv')  # convert
+#surface_to_entities = sfi_to_ste(base_dir + 'surfaceFormIndex.csv')  # convert
+#save(surface_to_entities, base_dir + 'surface_to_entities.pickle')
 
-#save(surfaceToEntities, base_dir + 'surfaceToEntities.pickle')
-surfaceToEntities = load(base_dir + 'surfaceToEntities.pickle')
-print("LOAD")
-print(time.time() - start)
-#ng = NGram([surfaceForm for surfaceForm in surfaceToEntities.keys()])
-for labels in labelColumn:
-    # generate candidates
-    entity_candidates = generate_candidates(labels, surfaceToEntities, 0)  # ng
-    print("candidates")
-    print(time.time() - start)
+surface_to_entities = load(base_dir + 'surface_to_entities.pickle')
+print("LOAD", time.time() - start)
+ng = NGram([surface_form for surface_form in surface_to_entities.keys()])
+print("LOAD NGRAM", time.time() - start)
+for labels in label_column:
+    begin = time.time()
+    entity_candidates = generate_candidates(labels, surface_to_entities, ng)
+    print("CANDIDATES", len(entity_candidates))
 
-    #save(entity_candidates, base_dir + 'candidates.pickle')
-    #entity_candidates = load(base_dir + 'candidates.pickle')
-
-    # load embeddings and create disambiguation graph
     subset = create_subset(entity_candidates,
                            base_dir + 'dbpedia-embeddings-skipgram-300-filtered.txt',
                            base_dir + 'dbpedia-embeddings-skipgram-300-subset.txt')
     w2v = KeyedVectors.load_word2vec_format(subset, binary=False)
     disambiguation_graph, entity_candidates = disambiguate_entities(entity_candidates, w2v)
+    disambiguation_graph = add_edges(entity_candidates, disambiguation_graph, w2v)
+    personalization = normalize_priors(entity_candidates)
+    disambiguation_graph.remove_edges_from(thin_out(disambiguation_graph))
 
-    # add edges on the graph
-    disambiguation_graph = addEdges(entity_candidates, disambiguation_graph, w2v)
+    #
+    # NO ETP NEEDED SINCE NX.PAGERANK AUTOMATICALLY NORMALIZE USING THE SAME PAPER'S FORMULA
+    #
+    page_rank = nx.pagerank(disambiguation_graph, tol=1e-04, max_iter=50, alpha=0.9, personalization=personalization)
+    annotations += list(best_annotation(entity_candidates, page_rank))
+    done = time.time()
+    print("ANNOTATE", len(annotations), done - begin)
+    print("TOTAL TIME", time.time() - start)
 
-    # normalize priors
-    denominator = normalizePriors(entity_candidates)
-
-    # normalizza prob a priori
-    personalization = {node: prop['weight'] / denominator[surface_form] for surface_form in entity_candidates
-                       for (node, prop) in entity_candidates.get(surface_form).nodes(data=True)
-                       if denominator[surface_form] != 0}
-
-    # calcola denominatore per cos similarity
-    """denominator = {}
-    for v1 in disambiguation_graph.nodes():
-        denominator[v1] = 0
-        for v2 in disambiguation_graph.nodes():
-            if disambiguation_graph.get_edge_data(v1, v2) is not None:
-                denominator[v1] += disambiguation_graph.get_edge_data(v1, v2)['weight']"""
-
-    # aggiorna pesi archi
-    """for v1 in disambiguation_graph.nodes():
-        for v2 in disambiguation_graph.nodes():
-            if disambiguation_graph.get_edge_data(v1, v2) is not None:
-                disambiguation_graph.add_weighted_edges_from(
-                    [(v1, v2, etp(disambiguation_graph, v1, v2, denominator[v1]))])"""
-
-    # thin out 25% of edges
-    disambiguation_graph.remove_edges_from(thinOut(disambiguation_graph))
-
-    # pagerank
-    pageRank = nx.pagerank(disambiguation_graph, tol=1e-05, max_iter=50, alpha=0.9, personalization=personalization)
-
-    # pick the best candidate for each surface form and save all of them
-    annotations += list(bestAnnotation(entity_candidates, pageRank))
-    print("ANNOTATE")
-    print(time.time() - start)
 
 annotated_table["entity"] = annotations
 annotate(annotated_table, base_dir + 'annotations.csv')
